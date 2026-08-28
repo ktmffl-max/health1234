2b84897754a76ad88a03f08cd989f9ada1d60359
ktmffl <ktmffl@gmail.com>
2026-08-28 09:54:12 +0900

    운동프로그램 갱신 2026-08-28  9:54:12.50

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "b84aa37f4f64";
+const VERSION = "64bfa02a76ab";
 const CACHE = "workout-" + VERSION;
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
                 "./icon-192.png", "./icon-512.png",
